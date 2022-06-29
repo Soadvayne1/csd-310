8e1dfdde0495b7477a954fd28dd56ade0994ac96
Soadvayne1 <97357392+Soadvayne1@users.noreply.github.com>
2022-06-28 21:38:45 -0400

    Update pytech_queries.py

--- module_5/pytech_queries.py
+++ module_5/pytech_queries.py
@@ -1,1 +1,25 @@
+'''
+Jeremy Johnson
+CYBR410
+Assignment 5.3
+6/28/2022
+'''
 
+from pprint import pprint
+from pymongo import MongoClient
+
+url = "mongodb+srv://admin:admin@cluster0.vj6wlif.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(url)
+db = client.pytech
+docs = db.students.find({})
+
+print("-- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
+
+for i in docs: 
+    print('Student ID: ' + i['student_id'] + '\n' + 'First Name: ' + i['first_name'] + '\n' + 'Last Name: ' + i['last_name'] + '\n')
+    
+print('-- DISPLAYING STUDENT DOCUMENT FROM find_one() QUERY --')
+
+doc = db.student.find_one({'student_id': '1009'})
+
+print('Student ID: ' + i['student_id'] + '\n' + 'First Name: ' + i['first_name'] + '\n' + 'Last Name: ' + i['last_name'] + '\n')
